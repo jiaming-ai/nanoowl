ad3f95032b7e8a6996256150572dd2b1d790939a
XP <dcsxpt@nus.edu.sg>
2024-05-29 17:05:42 +0800

    md

--- docker/23-01/run.sh
+++ docker/23-01/run.sh
@@ -3,14 +3,13 @@
 
 docker run \
     -it \
-    -d \
     --rm \
     --ipc host \
     --gpus all \
     --shm-size 14G \
-    --device /dev/video0:/dev/video0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -p 7860:7860 \
-    -v $(pwd):/nanoowl \
-    nanoowl:23-01
+    -v /home/user/projects/owl/nanoowl:/nanoowl \
+    nanoowl:23-01 \
+    bash
